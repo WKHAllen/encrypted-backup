8b18d92c967ab18874221c13e84a567376d2d219
WKHAllen <wkhallen@gmail.com>
2022-06-19 15:55:28 -0400

    Parse command line inputs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+use clap::{Parser, Subcommand};
+use glob::Pattern;
+use std::path::PathBuf;
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Backs up and encrypts files and directories
+    Backup {
+        /// Comma-separated paths to include in the backup
+        #[clap(multiple_values = true, value_delimiter = ',', value_parser = validate_path)]
+        include_paths: Vec<PathBuf>,
+        /// Comma-separated globs to exclude from the backup
+        #[clap(short, long, multiple_values = true, value_delimiter = ',', value_parser = validate_glob)]
+        exclude_globs: Vec<Pattern>,
+    },
+    /// Decrypts and extracts an encrypted backup
+    Extract {
+        /// Path to the encrypted backup
+        #[clap(value_parser = validate_file)]
+        path: PathBuf,
+    },
+}
+
+/// A tool to securely back up files and directories
+#[derive(Parser, Debug)]
+#[clap(about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+fn validate_file(path_str: &str) -> Result<PathBuf, String> {
+    let path = PathBuf::from(path_str);
+
+    if !path.exists() {
+        Err(format!("Path does not exist: {}", path_str))
+    } else if !path.is_file() {
+        Err(format!("Path is not a file: {}", path_str))
+    } else {
+        Ok(path)
+    }
+}
+
+fn validate_path(path_str: &str) -> Result<PathBuf, String> {
+    let path = PathBuf::from(path_str);
+
+    if !path.exists() {
+        Err(format!("Path does not exist: {}", path.display()))
+    } else if !path.is_dir() && !path.is_file() {
+        Err(format!(
+            "Path is not a file or directory: {}",
+            path.display()
+        ))
+    } else {
+        Ok(path)
+    }
+}
+
+fn validate_glob(glob_str: &str) -> Result<Pattern, String> {
+    let glob_result = Pattern::new(glob_str);
+
+    match glob_result {
+        Ok(glob_pattern) => Ok(glob_pattern),
+        Err(_e) => Err(format!("Invalid glob: {}", glob_str)),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    dbg!(cli);
 }
